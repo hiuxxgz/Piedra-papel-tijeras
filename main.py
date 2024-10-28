98aeaf94937313ba92e1f19b2ade668b7cc4a8ee
hiuxxgz <hgcastro27@gmail.com>
2024-10-28 15:07:17 +0100

    nuevo

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hola mundo")
+import random
+opciones=("piedra","papel","tijeras")
+def juego():
+    persona=input("Elige una de estas tres opciones:piedra,papel,tijeras")
+    if persona!=opciones:
+        print("Escoge una de esas tres no hagas tonterías")
+        return
+   
+juego()      
+
