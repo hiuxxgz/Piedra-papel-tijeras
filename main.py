92fa627db3cf64782eed333db625e0d81eefa423
hiuxxgz <hgcastro27@gmail.com>
2024-11-04 14:53:38 +0100

    acabado sin variables

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import random
 opciones=("piedra","papel","tijeras")
 def juego():
-    persona=input("Elige una de estas tres opciones:piedra,papel,tijeras\n")
-    
-    if persona not in opciones:
-        print("Escoge una de esas tres no hagas tonterías")
-        return
-    
-    ordenador=random.choice(opciones)
-    print("Elijo",ordenador)
-    if ordenador==persona:
-        print("Hemos epatado,la próxima vez no será así")
-        return
-    elif(persona=="piedra"and ordenador=="tijeras") or (persona=="papel" and ordenador=="piedra") or (persona=="tijeras"and ordenador=="papel"):
-        print("Felicidades me ganaste,la próxima no será así")
-        return
+    puntos_persona=0
+    puntos_ordenador=0
+    while puntos_persona<3 and puntos_ordenador<3:
+        print(puntos_persona,"-",puntos_ordenador)
+        persona=input("Elige una de estas tres opciones:piedra,papel,tijeras\n")
+       
+        if persona not in opciones:
+            print("Escoge una de esas tres no hagas tonterías")
+            continue
+            
+        ordenador=random.choice(opciones)
+        print("Elijo",ordenador) 
+
+        if ordenador==persona:
+            print("Hemos empatado,la próxima vez no será así",puntos_persona,"-",puntos_ordenador)  
+        elif(persona=="piedra"and ordenador=="tijeras") or (persona=="papel" and ordenador=="piedra") or (persona=="tijeras"and ordenador=="papel"):
+            puntos_persona+=1
+            print("Felicidades me ganaste,la siguiente ya te ganaré")
+        else:
+            puntos_ordenador+=1
+            print("Perdiste,la próxima vez será")
+
+    if puntos_persona==3:
+        print("Pero bueno, dicen que lo importante es participar")
     else:
-        print("Perdiste la próxima vez será")
-        return
+        print("Mientras experimenta el sabor de la derrota JA JA JA")
 juego()      
 
